73c9574df5d59c3c1ded725b62d7e018ceedb28f
chaiquan-j <1700159418@qq.com>
2020-09-16 17:23:04 +0800

    0.0.0
    all

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入rem适配
+import 'amfe-flexible';
+
+// 引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+// 清除默认样式
+import 'normalize.css'
+
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/start.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="start">
+		<div class="start_body">
+			<h1>启动页</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+	.start{
+		height: 100%;
+		width: 100%;
+		
+		.start_body{
+			height: 100%;
+			width: 100%;
+		}
+	}
+</style>
