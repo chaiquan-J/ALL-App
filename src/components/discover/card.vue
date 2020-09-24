f672eed3d85b5407e0f09bb94fc88017ebfd3e58
chaiquan-J <1700159418@qq.com>
2020-09-24 23:40:00 +0800

    0.0
    发现

---
+++ src/components/discover/card.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="card">
+		<div class="content">
+			<div class="shopp_box">
+				<template v-for="(item, index) in card">
+					<div class="cont_box">
+						<img :src="item.src" alt="" />
+						<span class="desc">{{ item.desc }}</span>
+					</div>
+				</template>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	props: ['card'],
+	methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+.card {
+	height: 1350px;
+	width: 100%;
+	position: relative;
+	z-index: 10;
+	border-bottom: 7px solid #E2E4EA;
+
+	.content {
+		height: 100%;
+		width: 670px;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+
+		.shopp_box {
+			width: 670px;
+			height: auto;
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-between;
+			margin-top: 31px;
+
+			.cont_box {
+				height: 315px;
+				width: 301px;
+				overflow: hidden;
+				display: flex;
+				flex-direction: column;
+				align-items: flex-start;
+				margin-bottom: 20px;
+
+				img {
+					height: 176px;
+					width: 100%;
+					margin-bottom: 20px;
+				}
+
+				.desc {
+					display: block;
+					width: 300px;
+					height: auto;
+					text-align: left;
+					line-height: 30px;
+					font-family: 'PingFangSC-Semibold';
+					font-size: 20px;
+					color: #333333;
+					font-weight: 700;
+				}
+			}
+		}
+	}
+}
+</style>
--- src/views/discover.vue
+++ src/views/discover.vue
@@ -2,7 +2,7 @@
 	<div class="discover">
 		<div class="discover_body">
 			<!-- 顶部导航栏 -->
-			<van-nav-bar class="navbar" z-index="99" :border="false" fixed placeholder>
+			<van-nav-bar class="navbar" z-index="99" :border="false" placeholder>
 				<template #title>
 					<span>发现</span>
 				</template>
@@ -10,23 +10,14 @@
 			<!-- 轮播 -->
 			<div class="advertising_swip">
 				<van-swipe class="my-swipe" :autoplay="3000" indicator-color="#a76f6f00">
-					<van-swipe-item>
-						<div class="img_box"><img src="../assets/img/discover/E099F29270B08DD8B468CE5F5E2D7612.png" alt="" /></div>
-					</van-swipe-item>
-					<van-swipe-item>
-						<div class="img_box"><img src="../assets/img/discover/E099F29270B08DD8B468CE5F5E2D7612.png" alt="" /></div>
-					</van-swipe-item>
-					<van-swipe-item>
-						<div class="img_box"><img src="../assets/img/discover/E099F29270B08DD8B468CE5F5E2D7612.png" alt="" /></div>
-					</van-swipe-item>
-					<van-swipe-item>
-						<div class="img_box"><img src="../assets/img/discover/E099F29270B08DD8B468CE5F5E2D7612.png" alt="" /></div>
-					</van-swipe-item>
-					<van-swipe-item>
-						<div class="img_box"><img src="../assets/img/discover/E099F29270B08DD8B468CE5F5E2D7612.png" alt="" /></div>
-					</van-swipe-item>
+					<template v-for="index in 5">
+						<van-swipe-item>
+							<div class="img_box" :key="index"><img src="../assets/img/discover/E099F29270B08DD8B468CE5F5E2D7612.png" alt="" /></div>
+						</van-swipe-item>
+					</template>
 				</van-swipe>
 			</div>
+			<!-- 分类航栏 -->
 			<div class="nav_two">
 				<div class="content">
 					<template v-for="(item, index) in nav_two">
@@ -37,7 +28,45 @@
 					</template>
 				</div>
 			</div>
-			<div class=""></div>
+			<!-- 分类导航 -->
+			<div class="nav_three">
+				<van-grid class="nav_grid" :border="false">
+					<template v-for="(item, index) in nav_three">
+						<van-grid-item class="cont">
+							<div class="cont_box" :key="index">
+								<img :src="item.icon" alt="" />
+								<span>{{ item.text }}</span>
+							</div>
+						</van-grid-item>
+					</template>
+				</van-grid>
+			</div>
+			<!-- 每日一句 -->
+			<div class="sentence">
+				<div class="content">
+					<div class="everyday">
+						<div class="title">
+							每日一句
+							<van-icon name="arrow" size="10" color="#41485D" />
+						</div>
+						<span class="desc">this universe is not outside of you Look inside yourse…</span>
+						<div class="btn">查看图片</div>
+					</div>
+					<div class="FAQ">
+						<div class="title">
+							你问我答 日更
+							<van-icon name="arrow" size="10" color="#41485D" />
+						</div>
+						<span class="desc">wood=木头，knock on wood=？</span>
+						<div class="btn">查看答案</div>
+					</div>
+				</div>
+			</div>
+			<!-- 卡片 -->
+			<card :card="card"></card>
+			<!-- 底线 -->
+			<div class="bottom-divider"><van-divider :hairline="false">页面有尽头，学习无止境</van-divider></div>
+			<!-- 背景 -->
 			<div class="bg_color"></div>
 		</div>
 		<tab-bar></tab-bar>
@@ -46,6 +75,8 @@
 
 <script>
 import TabBar from '@/components/tabbar.vue';
+import Card from '@/components/discover/card.vue';
+
 export default {
 	data() {
 		return {
@@ -66,12 +97,65 @@ export default {
 					text: '每日一句',
 					icon: require('../assets/icon/sign.png')
 				}
+			],
+			nav_three: [
+				{
+					text: '考研资讯',
+					icon: require('../assets/icon/curriculum.png')
+				},
+				{
+					text: '经验帖',
+					icon: require('../assets/icon/book.png')
+				},
+				{
+					text: '小挑战',
+					icon: require('../assets/icon/trophy.png')
+				},
+				{
+					text: '话题',
+					icon: require('../assets/icon/gambit.png')
+				}
+			],
+			card: [
+				{
+					src: require('../assets/img/course/IMG_0330.png'),
+					desc: '会员免费 | 初学者60天刷完15部美剧'
+				},
+				{
+					src: require('../assets/img/course/B1AC5CE505834FB5CB9F244338796957.png'),
+					desc: '考研长难句 | 每天10分钟，攻克长难句'
+				},
+				{
+					src: require('../assets/img/course/BB6E6BB6C81AC962F0B4EFA502FD074E.png'),
+					desc: '基础音标课 丨每天15分钟，掌握基础发音'
+				},
+				{
+					src: require('../assets/img/course/72F51587B2475382650F7B7F73E7893E.png'),
+					desc: '编程基础课｜零基础学编程，新时代职场利器'
+				},
+				{
+					src: require('../assets/img/course/BB6E6BB6C81AC962F0B4EFA502FD074E.png'),
+					desc: '基础音标课 丨每天15分钟，掌握基础发音'
+				},
+				{
+					src: require('../assets/img/course/BB6E6BB6C81AC962F0B4EFA502FD074E.png'),
+					desc: '基础音标课 丨每天15分钟，掌握基础发音'
+				},
+				{
+					src: require('../assets/img/course/BB6E6BB6C81AC962F0B4EFA502FD074E.png'),
+					desc: '基础音标课 丨每天15分钟，掌握基础发音'
+				},
+				{
+					src: require('../assets/img/course/BB6E6BB6C81AC962F0B4EFA502FD074E.png'),
+					desc: '基础音标课 丨每天15分钟，掌握基础发音'
+				}
 			]
 		};
 	},
 	methods: {},
 	components: {
-		TabBar
+		TabBar,
+		Card
 	}
 };
 </script>
@@ -94,6 +178,14 @@ export default {
 			margin-right: 4px;
 		}
 	}
+
+	.nav_three {
+		.van-grid {
+			.van-grid-item__content {
+				background-color: #f9fafb !important;
+			}
+		}
+	}
 }
 </style>
 
@@ -105,6 +197,8 @@ export default {
 	.discover_body {
 		height: 100%;
 		width: 100%;
+		background-color: #f9fafb;
+		margin-bottom: 110px;
 
 		.bg_color {
 			// max-width: 375px; //按一倍图适配
@@ -137,7 +231,7 @@ export default {
 			height: 310px;
 			width: 100%;
 			position: relative;
-			z-index: 99;
+			z-index: 10;
 			margin-bottom: 28px;
 
 			.my-swipe {
@@ -160,31 +254,25 @@ export default {
 			}
 		}
 
+		// 导航栏
 		.nav_two {
 			height: 136px;
 			width: 100%;
 			position: relative;
-			z-index: 99;
+			z-index: 10;
+			margin-bottom: 54px;
 
 			.content {
 				height: 100%;
 				width: 670px;
 				margin: 0 auto;
-				background-color: #eee;
 				display: flex;
 				justify-content: space-between;
 
 				.cont_box:nth-child(1) {
 					.icon {
-						height: 108px;
-						width: 108px;
 						background: #ffa940;
 						box-shadow: 0 2px 4px 0 #ffa940;
-						border-radius: 100%;
-						margin-bottom: 5px;
-						display: flex;
-						align-items: center;
-						justify-content: center;
 					}
 
 					img {
@@ -195,15 +283,8 @@ export default {
 
 				.cont_box:nth-child(2) {
 					.icon {
-						height: 108px;
-						width: 108px;
 						background: #ff7e5d;
 						box-shadow: 0 2px 4px 0 #d64d4d;
-						border-radius: 100%;
-						margin-bottom: 5px;
-						display: flex;
-						align-items: center;
-						justify-content: center;
 					}
 
 					img {
@@ -220,15 +301,8 @@ export default {
 					align-items: center;
 
 					.icon {
-						height: 108px;
-						width: 108px;
 						background: #54cffe;
 						box-shadow: 0 2px 4px 0 #6ac6f2;
-						border-radius: 100%;
-						margin-bottom: 5px;
-						display: flex;
-						align-items: center;
-						justify-content: center;
 					}
 
 					img {
@@ -239,15 +313,8 @@ export default {
 
 				.cont_box:nth-child(4) {
 					.icon {
-						height: 108px;
-						width: 108px;
 						background: #6ac6f2;
 						box-shadow: 0 2px 4px 0 #54cffe;
-						border-radius: 100%;
-						margin-bottom: 5px;
-						display: flex;
-						align-items: center;
-						justify-content: center;
 					}
 
 					img {
@@ -262,7 +329,17 @@ export default {
 					display: flex;
 					flex-direction: column;
 					align-items: center;
-					
+
+					.icon {
+						height: 108px;
+						width: 108px;
+						border-radius: 100%;
+						margin-bottom: 5px;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+					}
+
 					span {
 						font-family: 'PingFangSC-Semibold';
 						font-size: 20px;
@@ -272,6 +349,192 @@ export default {
 					}
 				}
 			}
+		}
+
+		.nav_three {
+			height: 125px;
+			width: 100%;
+			position: relative;
+			z-index: 10;
+			margin-bottom: 25px;
+
+			.nav_grid {
+				height: 100%;
+
+				.cont:nth-child(1) {
+					img {
+						height: 29px;
+						width: 32px;
+						margin-bottom: 21px;
+					}
+				}
+
+				.cont:nth-child(2) {
+					img {
+						height: 26px;
+						width: 24px;
+						margin-bottom: 22px;
+					}
+				}
+
+				.cont:nth-child(3) {
+					img {
+						height: 30px;
+						width: 32px;
+						margin-bottom: 19px;
+					}
+				}
+
+				.cont:nth-child(4) {
+					img {
+						height: 34px;
+						width: 32px;
+						margin-bottom: 17px;
+					}
+				}
+
+				.cont_box {
+					height: 100%;
+					width: 100%;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					align-items: center;
+
+					span {
+						font-family: 'PingFangSC-Semibold';
+						font-size: 20px;
+						color: #333333;
+						font-weight: 700;
+						text-align: center;
+					}
+				}
+			}
+		}
+
+		// 每日一句
+		.sentence {
+			height: 297px;
+			width: 100%;
+			border-top: 14px solid #e2e4ea;
+			border-bottom: 14px solid #e2e4ea;
+			position: relative;
+			z-index: 10;
+
+			.content {
+				height: 100%;
+				width: 670px;
+				margin: 0 auto;
+				display: flex;
+				flex-direction: column;
+
+				.everyday {
+					height: auto;
+					width: 100%;
+					display: flex;
+					flex-direction: column;
+					margin-top: 19px;
+
+					.title {
+						height: 28px;
+						width: auto;
+						font-family: 'PingFangSC-Semibold';
+						font-size: 20px;
+						font-weight: 700;
+						color: #41485d;
+						display: flex;
+						align-items: center;
+						margin-bottom: 20px;
+					}
+
+					.desc {
+						height: 37px;
+						width: 100%;
+						font-family: 'PingFangSC-Regular';
+						font-size: 26px;
+						font-weight: 400;
+						color: #41485d;
+						text-align: left;
+						overflow: hidden;
+						text-overflow: ellipsis;
+						white-space: nowrap;
+						margin-bottom: 5px;
+					}
+
+					.btn {
+						height: 44px;
+						width: 142px;
+						background-image: linear-gradient(180deg, #ff805f 0%, #ff6d44 100%);
+						border-radius: 35.5px;
+						font-family: 'PingFangSC-Regular';
+						font-size: 20px;
+						font-weight: 400;
+						color: #ffffff;
+						text-align: center;
+						line-height: 43px;
+						margin-left: 530px;
+					}
+				}
+
+				.FAQ {
+					height: auto;
+					width: 100%;
+					display: flex;
+					flex-direction: column;
+					margin-top: 5px;
+
+					.title {
+						height: 28px;
+						width: auto;
+						font-family: 'PingFangSC-Semibold';
+						font-size: 20px;
+						font-weight: 700;
+						color: #41485d;
+						display: flex;
+						align-items: center;
+						margin-bottom: 20px;
+					}
+
+					.desc {
+						height: 37px;
+						width: 100%;
+						font-family: 'PingFangSC-Regular';
+						font-size: 26px;
+						font-weight: 400;
+						color: #41485d;
+						text-align: left;
+						overflow: hidden;
+						text-overflow: ellipsis;
+						white-space: nowrap;
+						margin-bottom: 5px;
+					}
+
+					.btn {
+						height: 44px;
+						width: 142px;
+						background-image: linear-gradient(180deg, #ff805f 0%, #ff6d44 100%);
+						border-radius: 35.5px;
+						font-family: 'PingFangSC-Regular';
+						font-size: 20px;
+						font-weight: 400;
+						color: #ffffff;
+						text-align: center;
+						line-height: 43px;
+						margin-left: 530px;
+					}
+				}
+			}
+		}
+
+		.bottom-divider {
+			margin: 0 auto;
+			width: 670px;
+			margin-top: 102px;
+			margin-bottom: 57px;
+			font-family: "PingFangSC-Regular";
+			font-weight: 400;
+			font-size: 20px;
+			color: #C5CAD5;
 		}
 	}
 }
